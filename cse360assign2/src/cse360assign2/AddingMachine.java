d32b1783497b79f6f2c82d6d916f5cdca8cbe708
virajnimmagadda <virajnimmagadda@10.183.180.143>
2019-10-06 17:13:41 -0700

    Initial commit of the AddingMachine.java

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,50 @@
+/**
+ * Viraj Nimmagadda, CSE 360 Monday, Assignment1
+ * This class has is a basic calculator that can add and subtract, clear and show the answer.
+ * It has 5 methods that 
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * this method will add to the current total
+	 * @param value is the value that will be added to the total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * this method will subtract from the current total
+	 * @param value is the value that will be subtracted.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * this method will return a string or all the calculations until the clear method is used.
+	 */
+	public String toString () {
+		return "";
+	}
+
+
+	/**
+	 * this method will clear the history.
+	 */
+	public void clear() {
+	
+	}
+}
