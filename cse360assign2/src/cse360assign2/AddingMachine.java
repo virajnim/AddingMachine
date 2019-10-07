66dbfcf67fec598bb91253ce419e75ba54e072c5
virajnimmagadda <virajnimmagadda@10.157.17.213>
2019-10-07 13:36:41 -0700

    updated the notes and last two methods

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -8,7 +8,7 @@ package cse360assign2;
 
 public class AddingMachine {
 	private int total;
-	private String history;
+	private String history = "0";
 	
 	/**
 	 * this is the constructor method used to create a new object to call.
@@ -31,7 +31,7 @@ public class AddingMachine {
 	 */
 	public void add (int value) {
 		total += value;
-		history += "+ " + value;
+		history += " + " + value;
 	}
 	
 	/**
@@ -40,22 +40,21 @@ public class AddingMachine {
 	 */
 	public void subtract (int value) {
 		total -= value;
-		
-		history += "- " + value;
+		history += " - " + value;
 	}
 		
 	/**
-	 * this method will return a string or all the calculations until the clear method is used.
+	 * this method will return a string of all the calculations until the clear method is used.
 	 */
 	public String toString () {
-		return "0 "+ history;
+		return history;
 	}
 
-
 	/**
-	 * this method will clear the history.
+	 * this method will clear the history and the total variables back to 0.
 	 */
 	public void clear() {
-		history = "";
+		history = "0";
+		total = 0;
 	}
 }
